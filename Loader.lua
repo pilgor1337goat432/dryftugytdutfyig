cf6f875822784f5d6008c7f8efda4d5725261087
pilgor1337goat432 <zarinovmatros@gmail.com>
2026-08-03 21:41:22 +0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,5 @@
--- ВСТАВЛЯТЬ
+-- загрузчик
+-- инициализация между logic.lua и ui.lia
 _G.WindowsXP_Share = {
     CurrentSpeed = 16,
     CurrentJump = 50,
@@ -10,19 +11,25 @@ _G.WindowsXP_Share = {
 }
 
 local function loadScript(url)
-    local success, content = pcall(function() return game:HttpGet(url) end)
+    local finalUrl = url .. "?t=" .. tostring(os.time()) -- обход кэша
+    local success, content = pcall(function() return game:HttpGet(finalUrl) end)
+    
     if success and content then
+        if string.find(content, "404: Not Found") then
+            warn("[XP Loader] Ошибка: Файл не найден на GitHub!")
+            return
+        end
         local func, err = loadstring(content)
-        if func then task.spawn(func) else warn("Ошибка Lua: " .. tostring(err)) end
+        if func then task.spawn(func) else warn("[XP Loader] Ошибка: " .. tostring(err)) end
     else
-        warn("Ошибка скачивания: " .. url)
+        warn("[XP Loader] Ошибка загрузки: " .. url)
     end
 end
 
--- КАЛтент админки
+-- ссылки
 local URL_LOGIC = "https://raw.githubusercontent.com/pilgor1337goat432/dryftugytdutfyig/refs/heads/main/logic.lua"
 local URL_UI = "https://raw.githubusercontent.com/pilgor1337goat432/dryftugytdutfyig/refs/heads/main/ui.lua"
 
 loadScript(URL_LOGIC)
-task.wait(0.1) -- безопастный интервал
+task.wait(0.5) -- безопасная пауза, чтобы логика успела прогрузиться
 loadScript(URL_UI)
